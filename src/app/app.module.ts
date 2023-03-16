6221fb457092df5eea56752c899b94fb446894eb
Shozib <mshozib000@gmail.com>
2023-03-16 18:04:57 +0600

    new-design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HealthPackageComponent } from './health-package/health-package.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HealthPackageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/health-package/health-package.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-package',
+  templateUrl: './health-package.component.html',
+  styleUrls: ['./health-package.component.css']
+})
+export class HealthPackageComponent {
+
+}
